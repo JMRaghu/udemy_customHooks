24ed714f1b3450dc2f91e6bf9c7209fd86c6f29b
Raghu Gowda <raghujm0291@gmail.com>
2020-04-05 00:50:38 +0530

    useEffect,useCallback,useRef,HandlingErrormessage, and loading

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,21 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from'./IngredientList'
+import ErrorModel from '../UI/ErrorModal'
 
 const  Ingredients = () =>{
-  const [userIngredients, setuserIngredients] = useState([])
+  const [userIngredients, setuserIngredients] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+  const [error,setError] = useState();
 
+ /*  useEffect(()=>{
+    fetch('https://ingredients-bd209.firebaseio.com/ingredients.json')
+    .then(response =>response.json())
+    .then(responseData =>{
+      const loadIngredients = [];
+      for(const key in responseData){
+      loadIngredients.push({
+        id : key,
+        title:responseData[key].title,
+        amount:responseData[key].amount
+      });
+    }
+    setuserIngredients(loadIngredients);//update the state
+    })
+  },[]);//[] specify dependencies acts like componentDidMount(called only once) */
+  //we are calling this useeffect in search componect so get ridding of this extra render cycle
+
+  useEffect (()=>console.log('Rendering arguments',userIngredients) ,[userIngredients] )
+  //the useEffect will run when there is a change in [userIngredients]
+    
   const addIngredientsHandler = (ingredientsFromIngredientsForm) =>{
+    setIsLoading(true);
     fetch('https://ingredients-bd209.firebaseio.com/ingredients.json',{
       method:'POST',
       body:JSON.stringify(ingredientsFromIngredientsForm),
       headers:{'Content-Type':'application/json'}
       //fetch always has promises
     }).then(response =>{
+      setIsLoading(false);
       return response.json() //will be converted to js object
-      }).then(responseData =>{ //responseData will be object now
+      }).then(responseData =>{ //responseData will be obj  ect now
         setuserIngredients(prevIngredients=>[
           ...prevIngredients,
           {id:responseData.name,...ingredientsFromIngredientsForm}
@@ -23,18 +48,45 @@ const  Ingredients = () =>{
           ])
       })
   }
+  //Parent component gets loaded because first time when we load data we call 'onFilterIngredients'
+  //which calls 'getFilterIngredients' in that we are calling 'setuserIngredients' which changes the state
+  //again rerenders component which means that it is creating 'getFilterIngredients'function bcoz entire components reruns
+  //science it is javascript function still 'getFilterIngredients' reruns for 2nd time so receives new value
+  // useCallback??
+//to avoid the above rerender useCallback wrap the function, so it caches the function
+//it is not recreated or run again
+  const getFilterIngredients =
+  useCallback((filterData) =>{
+    console.log('filterdata'+filterData)
+    setuserIngredients(filterData);
+  },[])
 
   const removeIngredients = ingredientsIDFromIngredientsForm =>{
+    setIsLoading(true);
+    fetch(`https://ingredients-bd209.firebaseio.com/ingredients/${ingredientsIDFromIngredientsForm}.json`,{
+      method:'DELETE',
+      //fetch always has promises
+    }).then(response =>{
+      setIsLoading(false);
     setuserIngredients(prevIngredients =>
       prevIngredients.filter(ingredient => ingredient.id !== ingredientsIDFromIngredientsForm)
     )
+    }).catch(error =>{
+      setError(error.message)
+      //setIsLoading(false);
+    })
+  }
+  const onClickErrorOkay = () =>{
+    setError(null);
+    setIsLoading(false);
   }
   return (
-    <div className="App">
-      <IngredientForm onAddIngredients={addIngredientsHandler} />
+      <div className="App">
+        {error && <ErrorModel onClose={onClickErrorOkay}>{error}</ErrorModel>}
+      <IngredientForm onAddIngredients={addIngredientsHandler} loading={isLoading} />
 
       <section>
-        <Search />
+        <Search onFilterIngredients={getFilterIngredients} />
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredients} />
       </section>
     </div>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const {onFilterIngredients} = props
+  const[useEnteredText,useSetEnteredText] =  useState('')
+  const inputRef = useRef();
+
+  useEffect(() => {
+      const timer = setTimeout(()=>{
+        if(useEnteredText === inputRef.current.value){
+          const query =
+            useEnteredText.length === 0
+                ? ''
+                : `?orderBy="title"&equalTo="${useEnteredText}"`;
+            fetch('https://ingredients-bd209.firebaseio.com/ingredients.json'+query)
+            .then(response=>response.json())
+            .then(responseData=>{
+              console.log('-----',responseData)
+              const loadIngredients=[];
+              for(const key in responseData){
+                loadIngredients.push({
+                  id:key,
+                  title:responseData[key].title,
+                  amount:responseData[key].amount
+                })
+              }
+              onFilterIngredients(loadIngredients)
+            })
+        }
+      },500)
+      //return should always be a function unmount kind of things
+      //clean up all unwanted times and make app faster
+      return () =>{
+        clearTimeout(timer)
+      }
+  }, [useEnteredText,onFilterIngredients,inputRef])
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" ref={inputRef} value={useEnteredText} onChange={(event)=>{
+            useSetEnteredText(event.target.value)
+          }} />
         </div>
       </Card>
     </section>
